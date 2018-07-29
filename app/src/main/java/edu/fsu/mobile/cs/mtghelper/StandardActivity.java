5be9584c69c88817ae6df3e2233e0c306e4c0514
JTLascha <296577@gmail.com>
2018-07-29 19:54:30 -0400

    Changed all ids in CommanderActivity to be descriptive

--- app/src/main/java/edu/fsu/mobile/cs/mtghelper/StandardActivity.java
+++ app/src/main/java/edu/fsu/mobile/cs/mtghelper/StandardActivity.java
@@ -4,10 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class StandardActivity extends AppCompatActivity {
-
+    private int playerCount = 4; // update this in onCreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standard);
+
     }
 }
