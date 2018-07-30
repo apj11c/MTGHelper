0899c80d98baa1fb86e9d9ca5a401ed85946d75d
JTLascha <296577@gmail.com>
2018-07-29 22:57:42 -0400

    Added player class to help organize layout stuff.

--- app/src/main/java/edu/fsu/mobile/cs/mtghelper/CommanderActivity.java
+++ app/src/main/java/edu/fsu/mobile/cs/mtghelper/CommanderActivity.java
@@ -2,12 +2,32 @@ package edu.fsu.mobile.cs.mtghelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class CommanderActivity extends AppCompatActivity {
+    private int playerCount = 4; // Set this through an intent
+
+    private Player p[] = new Player[6];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_commander);
+        p[0].rl = (RelativeLayout) findViewById(R.id.P1Frame);
+       // p[1].rl = (RelativeLayout) findViewById(R.id.P2Frame);
+
+        for(int i = 0; i <1; i++) { // change the 1 to 6 after adding in all the frames
+            p[i].health = (TextView) p[i].rl.findViewById(R.id.HealthLabel);
+            p[i].energy = (TextView) p[i].rl.findViewById(R.id.EnergyLabel);
+            p[i].poison = (TextView) p[i].rl.findViewById(R.id.PoisonLabel);
+            p[i].health1 = (Button) p[i].rl.findViewById(R.id.health_1);
+            p[i].health10 = (Button) p[i].rl.findViewById(R.id.health_10);
+            p[i].energyUp = (Button) p[i].rl.findViewById(R.id.energyUp);
+            p[i].energyDown = (Button) p[i].rl.findViewById(R.id.energyDown);
+            p[i].poisonUp = (Button) p[i].rl.findViewById(R.id.poisonUp);
+            p[i].lifegain = (Button) p[i].rl.findViewById(R.id.toggle_gain);
+        }
     }
 }
---
+++ app/src/main/java/edu/fsu/mobile/cs/mtghelper/Player.java
@@ -0,0 +1,18 @@
+package edu.fsu.mobile.cs.mtghelper;
+
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class Player {
+    public RelativeLayout rl;
+    public TextView health;
+    public TextView energy;
+    public TextView poison;
+    public Button health1;
+    public Button health10;
+    public Button energyUp;
+    public Button energyDown;
+    public Button poisonUp;
+    public Button lifegain;
+}
